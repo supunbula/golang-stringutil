12dcd8596122219e6b5483d613f7c13b1dd0af7a
sahirug <sahirug@gmail.com>
2022-06-27 19:09:59 +0530

    Update reverse.go

--- reverse.go
+++ reverse.go
@@ -18,6 +18,7 @@ limitations under the License.
 package stringutil
 
 // Reverses returns its argument string reversed rune-wise left to right.
+// ### this is a new comment
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
